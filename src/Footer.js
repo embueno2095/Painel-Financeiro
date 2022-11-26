886bfa95856b404cc504a6da142083c56627a7f5
Eduardo Bueno <tec.ebueno@gmail.com>
2022-11-26 17:01:41 -0300

    Ajuste Fotter

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,10 @@ export default class Footer extends Component {
     return (
 <div>
   <footer className="main-footer">
-    <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright © 2022 <a href="#">Painel Resumo Financeiro</a>.</strong>
     All rights reserved.
     <div className="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
+      <b>Version</b> 0.0.1
     </div>
   </footer>
 </div>
